014e9e3ae70972f19173dac65bfec7899ee0563d
Tanner Morse <tannermorse@tanners-mbp.mtc.byu.edu>
2018-06-05 17:03:58 -0600

    Added the data models for the individual

---
+++ Individual+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Individual+CoreDataClass.swift
+//  StarWars
+//
+//  Created by Tanner Morse on 6/5/18.
+//  Copyright © 2018 Tanner Morse. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Individual)
+public class Individual: NSManagedObject {
+
+}
